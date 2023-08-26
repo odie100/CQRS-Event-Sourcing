ef2e76b6128f3f46de0447b8fb5cafca2232fdb9
Odie100 <andriampeno.odilon@gmail.com>
2023-08-26 11:07:55 +0300

    fix: pom.xml

---
+++ src/test/java/com/unidev/cqrs/CqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unidev.cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
